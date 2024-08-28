58dce0bb26b033a11f314e539bad5c49f306d132
yinjiao <fyj121322@gmail.com>
2024-08-28 12:10:19 -0700

    add script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div>
+    <header>
+      <h1>Hello from App</h1>
+      <h1>JavaScript Refresher</h1>
+    </header>
+
+    <ul>
+      <li>Base Syntax & Rules</li>
+      <li>Variables, Values & Operators</li>
+      <li>Functions</li>
+      <li>Objects</li>
+      <li>Arrays</li>
+      <li>Control Structures</li>
+      <li>Browser APIs & The DOM</li>
+      <li>Essential Features Used By React</li>
+      <li>Tricky Parts</li>
+    </ul>
+    </div>
   );
 }
 
