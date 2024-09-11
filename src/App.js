ba1c23b14933498056e60da21f93dfd2d563ab79
yinjiao <fyj121322@gmail.com>
2024-09-11 15:08:28 -0700

    u

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { apiKey } from "./util";
-function App() {
-  const hobbies = ["Sports", "Cooking"];
-  hobbies.map((item) => item + "!")
-  return (
-    <div>
+function Header() {
+  return(
     <header>
       <h1>Hello from App</h1>
       <h1>JavaScript Refresher</h1>
     </header>
+  )
+}
+function App() {
+  const hobbies = ["Sports", "Cooking"];
+  hobbies.map((item) => item + "!")
 
+  return (
+    <div>
+      <Header></Header>
     <ul>
       <li>Base Syntax & Rules</li>
       <li>Variables, Values & Operators</li>
